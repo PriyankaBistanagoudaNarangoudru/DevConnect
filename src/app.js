39b76defecbd83424bb3689af004b592778b7180
PriyankaNarangoudru <32214795+PriyankaBistanagoudaNarangoudru@users.noreply.github.com>
2026-03-14 23:27:23 +0530

    Delete and Update APIs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/signup', (req, res) => {
+    const user = new User(req.body);
+    user.save();
+    res.send('User added successfully!');
+})
+
 app.get('/getUserByEmail', async (req, res) => {
     try {
         const user =  await User.findOne({emailId: req.body.emailId});
@@ -24,10 +30,36 @@ app.get('/getUsers', async (req, res) => {
     }   
 })
 
-app.post('/signup', (req, res) => {
-    const user = new User(req.body);
-    user.save();
-    res.send('User added successfully!');
+app.delete('/deleteUser', async (req, res) => {
+    const userId = req.body._id;
+    try {
+        const users = await User.findByIdAndDelete(userId);
+        res.send('User deleted successfully');
+    } catch(err) {
+        res.status(400).send('Something went wrong');
+    }
+})
+
+app.patch('/updateUser', async (req, res) => {
+    const userId = req.body._id;
+    const data = req.body;
+    try {
+        const users = await User.findByIdAndUpdate(userId, data);
+        res.send('User updated successfully');
+    } catch {
+        res.status(400).send('Something went wrong');
+    }
+})
+
+app.patch('/updateUserByEmailId', async (req, res) => {
+    const emailId = req.body.emailId;
+    const data = req.body;
+    try {
+        await User.findOneAndUpdate({emailId: emailId}, data);
+        res.send('User updated successfully');
+    } catch (error) {
+        res.status(400).send('Something went wrong');
+    }
 })
 
 connectDB().then(() => {
